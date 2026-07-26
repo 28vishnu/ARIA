7a2dcac0c2fe0f8740b4a5afc70a8413ad6dc4ed
Vishnu Saketh Nanduri <129520592+28vishnu@users.noreply.github.com>
2026-07-26 22:47:40 +0530

    Update registery.py

--- actions/registery.py
+++
@@ -1,9 +0,0 @@
-from actions.manager import ActionManager
-from actions.actions.file import FileAction
-from actions.actions.notification import NotificationAction
-
-def create_default_action_manager() -> ActionManager:
-    manager = ActionManager(permission_mode="autonomous")
-    manager.register(FileAction())
-    manager.register(NotificationAction())
-    return manager
